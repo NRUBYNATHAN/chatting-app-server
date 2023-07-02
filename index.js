3d6031290b1d1c7165b3479f546b59f851768d86
NRUBYNATHAN <rubynathan999@gmail.com>
2023-07-02 12:26:02 +0530

    net

--- index.js
+++ index.js
@@ -9,7 +9,7 @@ const server = http.createServer(app);
 
 const io = new Server(server, {
   cors: {
-    origin: "http://localhost:3000",
+    origin: "https://marvelous-macaron-77c3d7.netlify.app/",
     methods: ["GET", "POST"],
   },
 });
